5679ac0f11da30bd4f9aa1c1ca913f0f0965d5f5
Kulsuma Khatun <kulsumakhatundewan@gmail.com>
2022-08-10 12:08:34 +0530

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,28 @@
+public class Book {
+    //attributes
+    String title;
+    int yearOfPublishing;
+    long iSBNNumber;
+    String authorName;
+
+
+    public static void main(String[] args) {
+        Book obj = new Book();
+        System.out.println("obj.title = " + obj.title);
+        System.out.println("obj.authorName = " + obj.authorName);
+        System.out.println("obj.yearOfPublishing = " + obj.yearOfPublishing);
+        System.out.println("obj.iSBNNumber = " + obj.iSBNNumber);
+        
+        obj.title="Complete Reference 12th Edition";
+        obj.authorName ="Herbert Schildth ";
+        obj.yearOfPublishing = 2021;
+        obj.iSBNNumber = 1234567890123L;
+
+        System.out.println("obj.title = " + obj.title);
+        System.out.println("obj.authorName = " + obj.authorName);
+        System.out.println("obj.yearOfPublishing = " + obj.yearOfPublishing);
+        System.out.println("obj.iSBNNumber = " + obj.iSBNNumber);
+
+    }
+    
+}
